a98b06c4c142ca22cea6b258301d2eb49fa38b86
shafi4js <shaficsedotjs@gmail.com>
2024-11-29 14:30:26 +0600

    this repo is the database of mini js examples

---
+++ datatype.js
@@ -0,0 +1,10 @@
+let stringId = "10123";
+const session = 2024; // the admission year
+stringId = session + stringId;
+
+console.log("new student id is ", stringId);
+
+let numericalId = 10123;
+numericalId = session + numericalId;
+
+console.log("new student id is ", numericalId);
